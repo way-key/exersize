d35b0dceaa57a96d1e31640e1b6624f4d855d503
植木　達也 <uekitatsuya@MacBook-Pro-5.local>
2020-03-16 17:18:34 +0900

    [Start]4-2

---
+++ Railsを学ぼう/sample_app/app/controllers/todolists_controller.rb
@@ -0,0 +1,43 @@
+class TodolistsController < ApplicationController
+  def new
+  	@list = List.new
+  end
+
+  def create
+  	#ストロングパラメーターを使用
+  	list = List.new(list_params)
+  	#DBへ保存する
+  	list.save
+  	redirect_to todolist_path(list.id)#詳細画面へリダイレクト
+  end
+
+  def index
+    @lists = List.all
+  end
+
+  def show
+    @list = List.find(params[:id])
+  end
+
+  def edit
+    @list = List.find(params[:id])
+  end
+
+  def update
+    list = List.find(params[:id])
+    list.update(list_params)
+    redirect_to todolist_path(list.id)
+  end
+  def destroy
+    list = List.find(params[:id])
+    list.destroy #データ（レコード）を削除
+    redirect_to todolists_path #List一覧画面へリダイレクト
+  end
+
+  private
+
+  def list_params
+  	params.require(:list).permit(:title, :body, :image)
+  end
+
+end
---
+++ Railsを学ぼう/sample_app/config/initializers/application_controller_renderer.rb
@@ -0,0 +1,10 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+
+Refile.secret_key = '30f43b85262a85552b67c1d576d6cc2b960152d95095367392b7d8148b595abb2b77a09615f5c95229a1a07fadcda38f9b11028c7008beb22b6e3b1b8b3ad995'
