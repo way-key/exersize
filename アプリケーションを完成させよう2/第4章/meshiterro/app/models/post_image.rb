6e70ae43bd04504eb5f44f91ba4b53cce8ea1bf1
way-key <45gqz7@gmail.com>
2020-03-22 15:25:24 +0900

    [Finish]5-4

---
+++ アプリケーションを完成させよう2/第4章/meshiterro/app/models/post_image.rb
@@ -0,0 +1,13 @@
+class PostImage < ApplicationRecord
+
+	 attachment :image# idは含めない
+	 belongs_to :user
+	 has_many :favorites, dependent: :destroy
+	 has_many :post_comments, dependent: :destroy
+	 validates :shop_name, presence: true
+	 validates :image, presence: true
+	 def favorited_by?(user)
+	 	favorites.where(user_id: user.id).exists?
+	 end
+
+end
---
+++ アプリケーションを完成させよう2/第4章/meshiterro/config/initializers/application_controller_renderer.rb
@@ -0,0 +1,10 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+
+Refile.secret_key = '0fead9469bc4961a2ebe08486644a920686f0e82f56a3b49a30b7e223e3c2332fd8019b7bed0823660c58680eea471897f7406469f669f535bd8e5789c42a2ac'
